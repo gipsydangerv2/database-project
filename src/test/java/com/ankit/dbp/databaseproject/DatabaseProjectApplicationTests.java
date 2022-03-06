b3609ebaff5ac5b19e50c6d87fc5c060cddd835f
kumaran2 <ankit2.kumar@intel.com>
2022-03-06 21:30:41 +0530

    initial commit

---
+++ src/test/java/com/ankit/dbp/databaseproject/DatabaseProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ankit.dbp.databaseproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatabaseProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
